a7ad3cb3f99fab1f36664c6e8f13dd06b4c370ba
Minsung Kim <alstjddl8@naver.com>
2020-09-26 05:08:49 +0900

    #

--- guestbook/src/main/java/kr/or/connect/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/kr/or/connect/guestbook/controller/GuestbookController.java
@@ -3,11 +3,14 @@ package kr.or.connect.guestbook.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +27,24 @@ public class GuestbookController {
 	
 	@GetMapping(path="/list")
 	public String list(@RequestParam(name="start", required=false, defaultValue="0") int start,
-					   ModelMap model) {
+					   ModelMap model, @CookieValue(value="count", defaultValue = "0", required = true) String value,
+					   HttpServletResponse response) {
+		
+		// 쿠키
+
+		try {
+			int i = Integer.parseInt(value);
+			value = Integer.toString(++i);
+		} catch (Exception e) {
+			value = "1";
+		}
+		
+		
+		Cookie cookie = new Cookie("count", value);
+		cookie.setMaxAge(60 * 60 * 24 * 365);	// 1년간 쿠키 유지
+		cookie.setPath("/");			// 경로 이하에 쿠키 적용
+		response.addCookie(cookie);
+		
 		
 		// start로 시작하는 방명록 목록 구하기
 		List<Guestbook> list = guestbookService.getGuestbooks(start);
@@ -47,6 +67,7 @@ public class GuestbookController {
 		model.addAttribute("list", list);
 		model.addAttribute("count", count);
 		model.addAttribute("pageStartList", pageStartList);
+		model.addAttribute("cookieCount", value);
 		
 		return "list";
 	}
