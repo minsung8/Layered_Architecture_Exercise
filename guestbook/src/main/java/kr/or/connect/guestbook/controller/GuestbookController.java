7ea719f0c43784919ab81a8b8381b92a09a5b7d6
Minsung Kim <alstjddl8@naver.com>
2020-09-26 05:50:39 +0900

    #

--- guestbook/src/main/java/kr/or/connect/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/kr/or/connect/guestbook/controller/GuestbookController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kr.or.connect.guestbook.dto.Guestbook;
 import kr.or.connect.guestbook.service.GuestbookService;
@@ -80,4 +82,52 @@ public class GuestbookController {
 		guestbookService.addGuestbook(guestbook, clientIp);
 		return "redirect:list";
 	}
+	
+	@GetMapping(path="/delete")
+	public String delete(@RequestParam(name="id", required=true) Long id,
+			@SessionAttribute("isAdmin") String isAdmin,
+			HttpServletRequest request,
+			RedirectAttributes redirectAttr
+			) {
+		
+		if (isAdmin == null || !"true".equals(isAdmin)) {
+			redirectAttr.addAttribute("errorMessage", "로그인을 하지 않았습니다.");
+			return "redirect:loginform";
+		} 
+		String clientIp = request.getRemoteAddr();
+		guestbookService.deleteGuestbook(id, clientIp);
+		
+		return "redirect:list";
+	}
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
